7db38318b5d57ae400eef32398b0ebd705a77c62
laszlototh82 <laszlo.toth@planet-itservices.com>
2019-05-21 16:36:10 +0200

    first commit

---
+++ src/functions.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: toth
+ * Date: 21.05.2019
+ * Time: 16:25
+ */
+
+namespace pachel\functions;
+
+/**
+ * Prüft, ob "/" hinten ist
+ * @param $dir
+ * @return string
+ */
+function checkSlash($dir){
+    if(mb_substr($dir,strlen($dir)-1,1) == "/"){
+        return $dir;
+    }
+    return $dir."/";
+}
+
+/**
+ * Ein String wird generiert aus Nummern, große und kleine Buchstaben
+ * @param int $lenght
+ * @return string
+ */
+function getRandomString($lenght = 10)
+{
+    $chars = [
+        0 => [65,90],//Großbuchstaben
+        1 => [97,122],//Kleinbuchstaben
+        2 => [48,57]//Nummer
+    ];
+    $string = "";
+    for($x=0;$x<$lenght;$x++)
+    {
+        $type = mt_rand(0,2);
+        $string .= chr(mt_rand($chars[$type][0],$chars[$type][1]));
+    }
+    return $string;
+}
+
+/**
+ * Ein String wird generiert aus Nummern, große und kleine Buchstaben
+ * @param int $lenght
+ * @return string
+ */
+function getUid($lenght = 32)
+{
+    $string = md5(time().microtime().mt_rand(0,9999));
+    if(strlen($string) > $lenght){
+        $string = substr($string,0,$lenght);
+    }
+    return $string;
+}
