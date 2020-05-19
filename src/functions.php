38f7ee060915ef106098612c7d91967fb9baf796
pachel82 <pachel82@gmail.com>
2020-05-19 11:14:28 +0200

    teszt

--- src/functions.php
+++ src/functions.php
@@ -75,3 +75,101 @@ function getSize($size)
     $size = $size/1024;
     return round($size,2)." Tb";
 }
+/**
+ * Prüft, ob "/" hinten ist
+ * @param $dir
+ * @return string
+ */
+if (!function_exists("checkSlash")) {
+
+    function checkSlash($dir) {
+        if (mb_substr($dir, strlen($dir) - 1, 1) == "/") {
+            return $dir;
+        }
+        return $dir . "/";
+    }
+
+}
+/**
+ * Ein String wird generiert aus Nummern, große und kleine Buchstaben
+ * @param int $lenght
+ * @return string
+ */
+if (!function_exists("getRandomString")) {
+
+    function getRandomString($lenght = 10) {
+        $chars = [
+            0 => [65, 90], //Großbuchstaben
+            1 => [97, 122], //Kleinbuchstaben
+            2 => [48, 57]//Nummer
+        ];
+        $string = "";
+        for ($x = 0; $x < $lenght; $x++) {
+            $type = mt_rand(0, 2);
+            $string .= chr(mt_rand($chars[$type][0], $chars[$type][1]));
+        }
+        return $string;
+    }
+
+}
+/**
+ * XOR encrypts a given string with a given key phrase.
+ *
+ * @param     string $InputString Input string
+ * @param     string $KeyPhrase   Key phrase
+ * @return    string    Encrypted string
+ */
+if (!function_exists("XOREncryption")) {
+
+    function XOREncryption($InputString, $KeyPhrase) {
+        $KeyPhraseLength = strlen($KeyPhrase);
+
+        // Loop trough input string
+        for ($i = 0; $i < strlen($InputString); $i++) {
+
+            // Get key phrase character position
+            $rPos = $i % $KeyPhraseLength;
+
+            // Magic happens here:
+            $r = ord($InputString[$i]) ^ ord($KeyPhrase[$rPos]);
+
+            // Replace characters
+            $InputString[$i] = chr($r);
+        }
+
+        return $InputString;
+    }
+
+}
+
+// Helper functions, using base64 to
+// create readable encrypted texts:
+if (!function_exists("xorEnc")) {
+
+    function xorEnc($InputString) {
+        if (!session_id()) {
+            die('error: no session! (xorEnc)');
+        }
+        $InputString .= '';
+        $InputString = XOREncryption($InputString, session_id());
+        $InputString = base64_encode($InputString);
+
+        return $InputString;
+    }
+
+}
+if (!function_exists("xorDec")) {
+
+    function xorDec($InputString) {
+        if (!session_id()) {
+            die('error: no session! (xorDec)');
+        }
+        $InputString .= '';
+        $InputString = base64_decode($InputString);
+        $InputString = XOREncryption($InputString, session_id());
+
+        return $InputString;
+    }
+
+}
+
