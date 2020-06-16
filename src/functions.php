4b80aa09b3a3bc6426300d7b14c65ae9f4ed219c
pachel82 <pachel82@gmail.com>
2020-06-16 11:02:35 +0200

    log

--- src/functions.php
+++ src/functions.php
@@ -1,79 +1,111 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: toth
  * Date: 21.05.2019
  * Time: 16:25
  */
-
 /**
  * Prüft, ob "/" hinten ist
  * @param $dir
  * @return string
  */
+if (!function_exists("checkSlash")) {
 
-function checkSlash($dir)
-{
-    if (mb_substr($dir, strlen($dir) - 1, 1) == "/") {
-        return $dir;
+    function checkSlash($dir) {
+        if (mb_substr($dir, strlen($dir) - 1, 1) == "/") {
+            return $dir;
+        }
+        return $dir . "/";
     }
-    return $dir . "/";
-}
 
+}
 /**
  * Ein String wird generiert aus Nummern, große und kleine Buchstaben
  * @param int $lenght
  * @return string
  */
-function getRandomString($lenght = 10)
-{
-    $chars = [
-        0 => [65,90],//Großbuchstaben
-        1 => [97,122],//Kleinbuchstaben
-        2 => [48,57]//Nummer
-    ];
-    $string = "";
-    for($x=0;$x<$lenght;$x++)
-    {
-        $type = mt_rand(0,2);
-        $string .= chr(mt_rand($chars[$type][0],$chars[$type][1]));
-    }
-    return $string;
-}
+if (!function_exists("getRandomString")) {
 
+    function getRandomString($lenght = 10) {
+        $chars = [
+            0 => [65, 90], //Großbuchstaben
+            1 => [97, 122], //Kleinbuchstaben
+            2 => [48, 57]//Nummer
+        ];
+        $string = "";
+        for ($x = 0; $x < $lenght; $x++) {
+            $type = mt_rand(0, 2);
+            $string .= chr(mt_rand($chars[$type][0], $chars[$type][1]));
+        }
+        return $string;
+    }
+
+}
+if (!function_exists("encodeString")) {
+
+    function encodeString($int) {
+        $h = 36;
+        $kesz = true;
+        while ($kesz) {
+            
+        }
+    }
+
+}
+if (!function_exists("legnagyobbHatvany")) {
+
+    function legnagyobbHatvany($int, $szamkor) {
+        //pow();
+    }
+
+}
+if (!function_exists("decodeString")) {
+
+    function encodeString($int, $lenght = 12) {
+        
+    }
+
+}
 /**
  * Ein String wird generiert aus Nummern, große und kleine Buchstaben
  * @param int $lenght
  * @return string
  */
-function getUid($lenght = 32)
-{
-    $string = md5(time().microtime().mt_rand(0,9999));
-    if(strlen($string) > $lenght){
-        $string = substr($string,0,$lenght);
-    }
-    return $string;
-}
+if (!function_exists("getUid")) {
 
-function getSize($size)
-{
-    if($size<1024){
-        return $size." bytes";
+    function getUid($lenght = 32) {
+        $string = md5(time() . microtime() . mt_rand(0, 9999));
+        if (strlen($string) > $lenght) {
+            $string = substr($string, 0, $lenght);
+        }
+        return $string;
     }
-    $size = $size/1024;
-    if($size<1024){
-        return round($size)." kB";
+
+}
+if (!function_exists("getSize")) {
+
+    function getSize($size) {
+        if ($size < 1024) {
+            return $size . " bytes";
+        }
+        $size = $size / 1024;
+        if ($size < 1024) {
+            return round($size) . " kB";
+        }
+        $size = $size / 1024;
+        if ($size < 1024) {
+            return round($size, 2) . " Mb";
+        }
+        $size = $size / 1024;
+        if ($size < 1024) {
+            return round($size, 2) . " Gb";
+        }
+        $size = $size / 1024;
+        return round($size, 2) . " Tb";
     }
-    $size = $size/1024;
-    if($size<1024){
-        return round($size,2)." Mb";
-    }
-    $size = $size/1024;
-    if($size<1024){
-        return round($size,2)." Gb";
-    }
-    $size = $size/1024;
-    return round($size,2)." Tb";
+
 }
 /**
  * Prüft, ob "/" hinten ist
@@ -172,4 +204,18 @@ if (!function_exists("xorDec")) {
     }
 
 }
+if (!function_exists("_log")) {
+    function _log($text) {
+        $logfile = __DIR__ . "/../logs/info.log";
+        if (!file_exists($logfile)) {
+            touch($logfile);
+        }
+        $logsize = filesize($logfile);
+        if ($logsize > (1024 * LOG_MAX_SIZE)) {
+            rename($logfile, $logfile . "." . date("Ymd") . ".log");
+            touch($logfile);
+        }
+        file_put_contents($logfile, "[" . date("Y-m-d H:i:s") . "]" . $text . "\n", FILE_APPEND);
+    }
 
+}
